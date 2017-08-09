fa46f37c41f4f4783ef3c0fc358a49f890815de1
Enzo <enzo.rp.90@gmail.com>
2017-08-09 17:34:50 -0500

    added client js, client webpack config, api js and readMe

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proyecto utilizando Reactjs y Redux
+
+## Descripcion
+> Aprendiendo Reactjs con Redux en el curso de Platzi
+
+## Dependencias Globales
+> Para que sea reconocido el comando **webpack**
+    `sudo npm i -g webpack`
+> Para el servidor de archivos estaticos
+    `sudo npm i -g serve`
--- source/server.js
+++ source/server.js
@@ -1,16 +1,44 @@
 import http from 'http'
 import React from 'react'
-import { renderToString } from 'react-dom/server'
+import { renderToString, renderToStaticMarkup } from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom'
+
+import Pages from './pages/containers/Page.jsx'
+import Layout from './pages/components/Layout.jsx'
+
 
 function requestHandle(request, response){
-    const html = renderToString(
-        React.DOM.h1(null, 'hola')
+    const context = {}
+
+    let html = renderToString(
+        <StaticRouter location={request.url} context={context}>
+            <Pages />
+        </StaticRouter>,
     )
 
-    response.write(html)
+    response.setHeader('Content-Type', 'text/html')
+
+    if (context.url) {
+        response.writeHead(301, {
+            Location: context.url,
+        })
+        response.end()
+    }
+
+    response.write(
+        renderToStaticMarkup(
+            <Layout
+                title="Aplicacion" 
+                content={html}
+            />
+        )
+    )
     response.end()
+
 }
 
 const server = http.createServer(requestHandle)
 
-server.listen(3000)
+server.listen(3000, function (){
+    console.log('Servidor escuchando en puerto 3000!')
+})
